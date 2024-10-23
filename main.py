e4c7a8b099c529405898cb10ffe1f296cc410b6e
ErotidesNeto <erotides.procurador@gmail.com>
2024-10-23 13:04:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -64,11 +64,8 @@ def main():
         if st.session_state.peticao_inicial_path:
             
             start_time = time.time()
-            with st.spinner('A IA está analisando'):
-                try:
-                    st.session_state.resultado_analise = analisa_peticao_inicial(st.session_state.peticao_inicial_path)
-                except ValueError as e:
-                    st.error(e)
+            with st.spinner('A IA está analisando'):                
+                st.session_state.resultado_analise = analisa_peticao_inicial(st.session_state.peticao_inicial_path)              
                 end_time = time.time()
                 st.session_state.analize_time = end_time - start_time                
                 st.success(f"Formulário preenchido pela IA em {st.session_state.analize_time:.2f} segundos!")
